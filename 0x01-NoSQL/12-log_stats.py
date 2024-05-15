c9c3b4735bd6a6bfd081fd98c886c0cd145d891b
Marcus Ngugi <marcusngugi@gmail.com>
2024-05-15 11:47:15 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
+"""Script that returns Nginx logs stats stored in MongoDB"""
+import pymongo
 
-"""a Python script that provides some
-stats about Nginx logs stored in MongoDB"""
+"""
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+database = client['logs']
+collection = database['nginx']
+
+logs = collection.count_documents({})
+print(f"{logs} logs")
+
+all_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+methods = {method: collection.count_documents(
+    {"method": method}) for method in all_methods}
+print("Methods:")
+for method, count in methods.items():
+    print(f"    method {method}: {count}")
+
+status = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"{status} status check")
+
+client.close()
+"""
 
 
-from pymongo import MongoClient
+def collection(db: dict) -> int:
+    """Function to retroeve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
+
+
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
+
+    print(f"{collection({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-
-    all_logs = collection.count_documents({})
-    get_logs = collection.count_documents({"method": "GET"})
-    post_logs = collection.count_documents({"method": "POST"})
-    put_logs = collection.count_documents({"method": "PUT"})
-    patch_logs = collection.count_documents({"method": "PATCH"})
-    delete_logs = collection.count_documents({"method": "DELETE"})
-    checks = collection.count_documents({"method": "GET", "path": "/status"})
-
-    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
-          " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
-          "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
+    main()
